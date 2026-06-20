35f1d6665b6bbcf002db890dc1509578ac07bea5
David Coleman <davidlc1229@gmail.com>
2026-06-20 00:55:15 -0700

    fix: extend watchFolders instead of replacing expo defaults

--- mobile/metro.config.js
+++ mobile/metro.config.js
@@ -6,10 +6,8 @@ const monorepoRoot = path.resolve(projectRoot, '..');
 
 const config = getDefaultConfig(projectRoot);
 
-// Include root node_modules in file watching
-config.watchFolders = [monorepoRoot];
+config.watchFolders = [...(config.watchFolders ?? []), monorepoRoot];
 
-// Resolve modules from mobile first, then root (where workspaced deps live)
 config.resolver.nodeModulesPaths = [
   path.resolve(projectRoot, 'node_modules'),
   path.resolve(monorepoRoot, 'node_modules'),
